cadf54f08afaa98e4a47432fd9914fe5db5c1832
Sergiu <patras.sergiu@yahoo.com>
2018-06-09 02:08:40 +0300

    peste tot

--- src/main/resources/static/scripts/main.js
+++ src/main/resources/static/scripts/main.js
@@ -38,7 +38,7 @@ function loadWeather(){
 		{
 			var myJSON = this.responseText;
 			var myObj = JSON.parse(myJSON);
-            var image = document.getElementById("iconvreme").src = "../public/sleet.png";			
+            var image = document.getElementById("iconvreme").src = "sleet.png";			
 ///var image = document.getElementById("iconvreme").src = "../icons/sleet.png";
             ///= "../icons/" + myObj.currently.icon + ".PNG";
 			document.getElementById("vreme").innerHTML = myObj.currently.temperature + "°C";

ac512f3c319a41d658d17b76a28fe18d08d83640
Sergiu <patras.sergiu@yahoo.com>
2018-06-09 02:23:53 +0300

    commit

--- src/main/resources/static/scripts/main.js
+++ src/main/resources/static/scripts/main.js
@@ -38,10 +38,19 @@ function loadWeather(){
 		{
 			var myJSON = this.responseText;
 			var myObj = JSON.parse(myJSON);
-            var image = document.getElementById("iconvreme").src = "/icons/sleet.png";			
+            var img = new Image();
+            var div = document.getElementById('iconvreme');
+            img.onload = function() {
+            div.appendChild(img);
+            };
+            img.src = '../icons/sleet.png';            
+
+            
+/*var image = document.getElementById("iconvreme").src = "../sleet.png";			
 ///var image = document.getElementById("iconvreme").src = "../icons/sleet.png";
             ///= "../icons/" + myObj.currently.icon + ".PNG";
-			document.getElementById("vreme").innerHTML = myObj.currently.temperature + "°C";
+*/			
+            document.getElementById("vreme").innerHTML = myObj.currently.temperature + "°C";
 		}
 
 	};

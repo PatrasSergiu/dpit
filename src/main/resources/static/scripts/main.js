bd264c8f4d18c4ed238f9fe067c531851941ba07
Sergiu <patras.sergiu@yahoo.com>
2018-06-09 03:02:13 +0300

    hm

--- src/main/resources/static/scripts/main.js
+++ src/main/resources/static/scripts/main.js
@@ -38,8 +38,7 @@ function loadWeather(){
 		{
 			var myJSON = this.responseText;
 			var myObj = JSON.parse(myJSON);
-            var image = document.getElementById("iconvreme");
-            iconvreme.src = "../icons/sleet.png"
+            document.getElementById('iconvreme').src = "../icons/sleet.png";
 
 ///var image = document.getElementById("iconvreme").src = "../icons/sleet.png";
             ///= "../icons/" + myObj.currently.icon + ".PNG";
